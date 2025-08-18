06dffbc24ad99e61b922d4056387e141961b64c9
aanahia <anahi2654@gmail.com>
2025-08-18 04:30:39 -0500

    Listar topicos sin y con parametros(curso y/o anio)

--- src/main/java/com/example/ForoHub/api/controller/TopicoController.java
+++ src/main/java/com/example/ForoHub/api/controller/TopicoController.java
@@ -2,6 +2,7 @@ package com.example.ForoHub.api.controller;
 
 import com.example.ForoHub.api.dto.DatosDetalleTopicoDTO;
 import com.example.ForoHub.domain.curso.CursoRepository;
+import com.example.ForoHub.domain.topico.DatosListaTopicoDTO;
 import com.example.ForoHub.domain.topico.DatosRegistroTopicoDTO;
 import com.example.ForoHub.domain.topico.Topico;
 import com.example.ForoHub.domain.topico.TopicoRepository;
@@ -10,6 +11,10 @@ import com.example.ForoHub.infra.exceptions.DuplicateResourceException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -42,5 +47,21 @@ public class TopicoController {
 
         var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new DatosDetalleTopicoDTO(topico));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DatosListaTopicoDTO>> listarMedicos(
+            @RequestParam(required=false) String curso,
+            @RequestParam(required=false) Integer anio,
+            @PageableDefault(size = 10, sort = {"fechaCreacion"}, direction = Sort.Direction.ASC) Pageable paginacion) {
+        if (curso != null && anio != null) {
+            var inicio = java.time.LocalDateTime.of(anio, 1, 1, 0, 0);
+            var fin = inicio.withMonth(12).withDayOfMonth(31).withHour(23).withMinute(59).withSecond(59);
+            var page = topicoRepository.findByCurso_NombreIgnoreCaseAndFechaCreacionBetween(curso, inicio, fin, paginacion)
+                    .map(DatosListaTopicoDTO::new);
+            return ResponseEntity.ok(page);
+        }
+        var page = topicoRepository.findAll(paginacion).map(DatosListaTopicoDTO::new);
+        return ResponseEntity.ok(page);
     }
 }
--- src/main/java/com/example/ForoHub/domain/topico/TopicoRepository.java
+++ src/main/java/com/example/ForoHub/domain/topico/TopicoRepository.java
@@ -1,7 +1,12 @@
 package com.example.ForoHub.domain.topico;
 
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
 
 public interface TopicoRepository extends JpaRepository <Topico, Long>{
     boolean existsByTituloAndMensaje(String titulo, String mensaje);
+    Page<Topico> findByCurso_NombreIgnoreCaseAndFechaCreacionBetween(
+            String nombreCurso, LocalDateTime inicio, LocalDateTime fin, Pageable pageable);
 }
